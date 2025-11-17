20e162b6fc315f9603475e20ae0c7645833e47f9
ANGELOREYES-afk <angelohenaoreyes@yahoo.com>
2025-11-16 22:08:15 -0600

    change config settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.Test_Path || "/Lucen"
 };
 
 export default nextConfig;
