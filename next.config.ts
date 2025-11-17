dd3f556e0710976210e3e6bcfc206299e88eb015
ANGELOREYES-afk <angelohenaoreyes@yahoo.com>
2025-11-16 22:09:42 -0600

    nevermind

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.Test_Path || "/Lucen"
 };
 
 export default nextConfig;
